350556c9682eb62235c9678d42abf2f8162c7f18
Mr Dipanshu Namdev <dipanshunamdev1312@gmail.com>
2024-06-09 00:41:30 +0530

    select single data from database.

--- springJDBC/src/main/java/com/springjdbc/App.java
+++ springJDBC/src/main/java/com/springjdbc/App.java
@@ -7,16 +7,14 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import com.springjdbc.dao.StudentDao;
 import com.springjdbc.entities.Student;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "My program started.....!");
-        
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-       
-        StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
-        
+public class App {
+	public static void main(String[] args) {
+		System.out.println("My program started.....!");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
+
 //        Student student = new Student();
 //        student.setId(666);
 //        student.setName("John");
@@ -34,11 +32,12 @@ public class App
 //        int change = studentDao.change(student);
 //        
 //        System.out.println("Changing Data : "+ student);
-        
-          
-          int delete = studentDao.delete(666);
-          
-          System.out.println("Deleting Data : "+delete);
-          
-    }
+//
+//		int delete = studentDao.delete(666);
+//
+//		System.out.println("Deleting Data : " + delete);
+
+		Student student = studentDao.getStudent(222);
+		System.out.println(student);
+	}
 }
--- springJDBC/src/main/java/com/springjdbc/dao/StudentDaoImpl.java
+++ springJDBC/src/main/java/com/springjdbc/dao/StudentDaoImpl.java
@@ -1,34 +1,44 @@
 package com.springjdbc.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.springjdbc.entities.Student;
 
-public class StudentDaoImpl implements StudentDao{
-	
+public class StudentDaoImpl implements StudentDao {
+
 	private JdbcTemplate jdbcTemplate;
-	
+
 	public int insert(Student student) {
 		// insert query
-		String query ="insert into student(id,name,city) values(?,?,?)";
+		String query = "insert into student(id,name,city) values(?,?,?)";
 		int result = this.jdbcTemplate.update(query, student.getId(), student.getName(), student.getCity());
 		return result;
 	}
-	
+
 	@Override
 	public int change(Student student) {
 		// Updating Data
 		String query = "update student set name=?, city=? where id=?";
-		int result = this.jdbcTemplate.update(query, student.getName(),student.getCity(),student.getId());
+		int result = this.jdbcTemplate.update(query, student.getName(), student.getCity(), student.getId());
 		return result;
 	}
-	
+
 	@Override
 	public int delete(int studentID) {
 		// Deleting data
 		String query = "DELETE FROM student WHERE id=?";
 		int delete = this.jdbcTemplate.update(query, studentID);
 		return delete;
+	}
+
+	@Override
+	public Student getStudent(int studentId) {
+		// Selecting single student data
+		String query = "Select * from student where id=?";
+		RowMapper<Student> rawMapper = new RawMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query,rawMapper, studentId);
+		return student;
 	}
 
 	public JdbcTemplate getJdbcTemplate() {
