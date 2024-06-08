f19e68fd88e1a9d22ec4fe73caeba53d58654fb2
Mr Dipanshu Namdev <dipanshunamdev1312@gmail.com>
2024-06-08 08:45:32 +0530

    Spring core

---
+++ springcore/src/main/java/com/springcore/App.java
@@ -0,0 +1,29 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println("hello.....");
+       ApplicationContext context = new ClassPathXmlApplicationContext("spconfig.xml");
+       
+       Student student1 = (Student) context.getBean("Student1");
+       Student student2 = (Student) context.getBean("Student2");
+       Student student3 = (Student) context.getBean("Student3");
+
+       
+       System.out.println(student1);
+       System.out.println(student2);
+       System.out.println(student3);
+
+       
+       
+    }
+}
