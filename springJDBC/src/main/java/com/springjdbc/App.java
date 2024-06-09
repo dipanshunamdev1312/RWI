68722bc7024bcf5fd9f4ea0e0426059341eae3ea
Mr Dipanshu Namdev <dipanshunamdev1312@gmail.com>
2024-06-09 13:06:08 +0530

    without .xml file with the help of java class.

--- springJDBC/src/main/java/com/springjdbc/App.java
+++ springJDBC/src/main/java/com/springjdbc/App.java
@@ -3,6 +3,7 @@ package com.springjdbc;
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -13,7 +14,7 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("My program started.....!");
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
 
 		StudentDao studentDao = context.getBean("studentDao", StudentDao.class);
 
